a183fca70f80a596565eb1f502fa3e092fa679b2
Dmitry <lupanov5@mail.ru>
2021-09-27 15:55:36 +0300

    Finished base framework functional

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,19 +1,20 @@
-import {$} from '../../core/core'
+import {$} from '../../core/dom'
 
 export class Excel {
     constructor(selector, options) {
-        this.$el = document.querySelector(selector)
+        this.$el = $(selector)
         this.components = options.components || []
     }
 
     getRoot() {
         const $root = $.create('div', 'excel')
 
-        this.components.forEach(Component => {
+        this.components = this.components.map(Component => {
             const $el = $.create('div', Component.className)
             const component = new Component($el)
-            $el.innerHTML = component.toHTML()
+            $el.html(component.toHTML())
             $root.append($el)
+            return component
         })
 
         return $root
@@ -21,5 +22,7 @@ export class Excel {
 
     render() {
         this.$el.append(this.getRoot())
+
+        this.components.forEach(component => component.init())
     }
 }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -2,6 +2,13 @@ import {ExcelComponent} from '../../core/ExcelComponent'
 
 export class Toolbar extends ExcelComponent {
     static className = 'excel__toolbar'
+
+    constructor($root) {
+        super($root, {
+            name: 'Toolbar',
+            listeners: ['click']
+        })
+    }
     toHTML() {
         return `
             <div class="button">
@@ -22,5 +29,9 @@ export class Toolbar extends ExcelComponent {
             <div class="button">
                 <i class="material-icons">format_underlined</i>
             </div>`
+    }
+
+    onClick(event) {
+        console.log(event.target)
     }
 }
